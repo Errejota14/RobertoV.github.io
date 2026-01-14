9fa3e73bc5b6919f6bc48a915b5a9258019d14ed
Roberto Velasquez <robertoj3055@gmail.com>
2026-01-14 16:05:25 -0400

    Configura base para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import { SITE_URL } from "./src/consts.ts";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://RobertoV.github.io",
+  site: "https://errejota14.github.io",
+  base: "/RobertoV.github.io",
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
