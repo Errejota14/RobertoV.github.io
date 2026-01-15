5018d56efe18088aa4ce7e8f0564bfe67e5cc667
Roberto Velasquez <robertoj3055@gmail.com>
2026-01-15 14:28:55 -0400

    MODIFICACIONlink

--- src/consts.ts
+++ src/consts.ts
@@ -53,10 +53,12 @@ export const about: About = {
   description:
     "Ingeniero Electrónico con experiencia en desarrollo y reparación de sistemas electrónicos a nivel de componentes, incluyendo microsoldadura y soldadura con cautín. Actualmente me desempeño en desarrollo electrónico y docencia universitaria, enfocado en la validación de sistemas y resolución de problemas técnicos. Experto en diseño de PCB, programación de microcontroladores (Arduino, ESP32, STM32) e integración hardware-software mediante PostgreSQL, FastAPI y Flask.",
 
-  links: [
-    { icon: "Github", href: "https://github.com/Errejota14" },
-    { icon: "Linkedin", href: "www.linkedin.com/in/roberto-velasquez3035" },
-  ],
+    links: [
+      { icon: "Github", href: "https://github.com/Errejota14" },
+      { icon: "Linkedin", href: "https://linkedin.com/in/roberto-velasquez3035/" },
+    ],
+    
+
 
   resumeHref:
     "https://drive.google.com/file/d/1rQQkr899ncNtkpyftp55BIcZmvvjv6j6/view?usp=sharing",
